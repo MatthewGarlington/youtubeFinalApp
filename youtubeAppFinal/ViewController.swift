64ffdccf950ed77aceb7fff8e39b69fc59c0319b
Matthew Garlington <sbmatthewg@gmail.com>
2020-06-06 06:24:02 -0400

    This is the initial Youtube App Creation project

---
+++ youtubeAppFinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtubeAppFinal
+//
+//  Created by Matthew Garlington on 6/6/20.
+//  Copyright © 2020 Matthew Garlington. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
